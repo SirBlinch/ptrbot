cc8f3317b7d0a124e8b0885b934555ebff8dc161
Himan <andre.aleks.andre@gmail.com>
2024-07-23 22:59:27 +0300

    Ничего не сделал, но много узнал

--- internal/userManager.go
+++ internal/userManager.go
@@ -1,17 +1,50 @@
 package internal
 
+import (
+	"database/sql"
+)
+
 type user struct {
 	id          int
 	state       int
-	accessLevel int
+	permissions string
 }
 
 var onlineUsers []user
 
-func addUser() {
+func addUser(userID int) {
+	db, err := sql.Open("sqlite3", "D:\\FromFlashCard\\FromLinux\\GO\\PTR_Bot\\DB_1_0.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	_, err = db.Exec("INSERT INTO Users (id,) VAIUES ($1,)", userID)
+	if err != nil {
+		panic(err)
+	}
+	_, err = db.Exec("INSERT INTO Users_Permissions (userID, PermissionID) VALUES ($1,0)", userID)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func toOnline() {}
+func toOnline(userID int) {
+	db, err := sql.Open("sqlite3", "D:\\FromFlashCard\\FromLinux\\GO\\PTR_Bot\\DB_1_0.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	var value int
+	err = db.QueryRow("SELECT COUNT(*) FROM Users WHERE id = ?", userID).Scan(&value)
+	if err != nil {
+		panic(err)
+	}
+	if value == 0 {
+		addUser(userID)
+	} else {
+
+	}
+}
 
 func toOffline() {}
